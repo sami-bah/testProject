476546de78edc6ad11c9c37396166cb7c10ac687
Sami Mirimiri <mirimiri_sami@bah.com>
2015-08-04 13:59:59 -0400

    much better looking from different generator

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('testProjectApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
